83b9a44d5e279162c945d21151e6f2ded11f1b4f
ImMau14 <newmauuelenoski02@gmail.com>
2025-08-22 12:50:06 -0400

    Refactor: Database and Auth controller refactorized

---
+++ backend/src/utils/hash_password.rs
@@ -0,0 +1,12 @@
+use argon2::{password_hash::{PasswordHasher, SaltString}, Argon2};
+use rand::rngs::OsRng;
+
+pub fn hash_password(password: &str) -> Result<String, String> {
+    let salt = SaltString::generate(&mut OsRng);
+    let argon2 = Argon2::default();
+
+    match argon2.hash_password(password.as_bytes(), &salt) {
+        Ok(hash) => Ok(hash.to_string()),
+        Err(e) => Err(format!("Password hash error: {}", e))
+    }
+}
--- backend/src/utils/mod.rs
+++ backend/src/utils/mod.rs
@@ -1,2 +1,9 @@
 pub mod get_array_of_sentences;
 pub use get_array_of_sentences::get_array_of_sentences;
+
+pub mod hash_password;
+pub use hash_password::hash_password;
+
+pub mod register_user;
+pub use register_user::register_user;
+pub use register_user::RegisterPayload;
