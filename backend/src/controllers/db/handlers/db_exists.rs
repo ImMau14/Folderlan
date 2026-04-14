de7df454e49724c5f410f1f89becfd7e959d3943
Mauricio Rodríguez <newmauuelenoski02@gmail.com>
2026-04-14 10:39:57 -0400

    Feat(backend): Improve database controller

---
+++ backend/src/controllers/db/handlers/db_exists.rs
@@ -0,0 +1,27 @@
+use crate::models::responses::ApiResponse;
+
+use actix_web::{Responder, web};
+use sqlx::SqlitePool;
+
+pub async fn db_exists(pool: web::Data<SqlitePool>) -> impl Responder {
+    let pool_ref: &SqlitePool = pool.get_ref();
+
+    let result = sqlx::query_scalar!(
+        r#"
+        SELECT EXISTS(
+            SELECT 1
+            FROM Users
+            WHERE role = 'owner' AND is_deleted = 0
+        ) as "exists!: i64"
+        "#
+    )
+    .fetch_one(pool_ref)
+    .await;
+
+    match result {
+        Ok(val) => ApiResponse::<()>::builder().exists(val == 1).ok(),
+        Err(e) => ApiResponse::<()>::builder()
+            .message(e.to_string())
+            .internal(),
+    }
+}
